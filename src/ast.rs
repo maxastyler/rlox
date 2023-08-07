bbd5f36ca93b18fe190b92dc859f530baacad213
Max Tyler <maxastyler@gmail.com>
2023-08-07 17:28:45 +0100

    Add expressions and stuff

--- src/ast.rs
+++ src/ast.rs
@@ -1,179 +1,49 @@
-use std::fmt::Debug;
-
-use nom::{character::is_space, And};
-
-#[derive(PartialEq)]
-pub enum Literal {
-    Nil,
-    False,
-    True,
-    Int(i64),
-    Double(f64),
-    Symbol(Symbol),
-}
-
-impl Debug for Literal {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Nil => f.write_str("nil"),
-            Self::False => f.write_str("false"),
-            Self::True => f.write_str("true"),
-            Self::Int(i) => f.write_fmt(format_args!("{}", i)),
-            Self::Double(d) => f.write_fmt(format_args!("{}", d)),
-	    Self::Symbol(s) => f.write_fmt(format_args!("{:?}", s))
-        }
-    }
-}
-
-#[derive(PartialEq)]
-pub enum Binary {
-    And(Vec<Expression>),
-    Plus(Vec<Expression>),
-}
-
-impl Debug for Binary {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let v = self.inner_v();
-        match v.len() {
-            0 => panic!("Binary with empty vector"),
-            1 => f.write_fmt(format_args!("({} {:?})", self.to_op_string(), v[0])),
-            _ => {
-                f.write_fmt(format_args!("({:?}", v[0]))?;
-                for x in v.iter().skip(1) {
-                    f.write_fmt(format_args!(" {} {:?}", self.to_op_string(), x))?;
-                }
-                f.write_str(")")
-            }
-        }
-    }
-}
-
-impl Binary {
-    pub fn ops() -> &'static [&'static str] {
-        &["and", "+"]
-    }
-
-    fn inner_v(&self) -> &Vec<Expression> {
-        match self {
-            Binary::And(v) => v,
-            Binary::Plus(v) => v,
-        }
-    }
-
-    pub fn from_op_string(s: &str, vec: Vec<Expression>) -> Self {
-        match s {
-            "and" => Binary::And(vec),
-            "+" => Binary::Plus(vec),
-            _ => panic!("Could not match operator string"),
-        }
-    }
-
-    pub fn to_op_string(&self) -> &'static str {
-        match self {
-            Binary::And(_) => "and",
-            Binary::Plus(_) => "+",
-        }
-    }
-
-    fn construct(f: fn(Vec<Expression>) -> Binary, mut v: Vec<Expression>) -> Expression {
-        match v.len() {
-            0 => panic!("Binary had length 0 vector"),
-            1 => v.remove(0).simplify(),
-            _ => Expression::Binary(Box::new(f(v.into_iter().map(|x| x.simplify()).collect()))),
-        }
-    }
-
-    pub fn simplify(self) -> Expression {
-        let (f, v): (fn(Vec<Expression>) -> Binary, Vec<_>) = match self {
-            Binary::And(v) => (Binary::And, v),
-            Binary::Plus(v) => (Binary::Plus, v),
-        };
-        Binary::construct(f, v)
-    }
-}
-
 #[derive(Debug, PartialEq)]
 pub struct Symbol(pub String);
 
 #[derive(Debug, PartialEq)]
-pub struct Parenthesised(pub Box<Expression>);
-
-#[derive(PartialEq)]
-pub enum Expression {
-    Literal(Literal),
-    Call(Symbol, Vec<Expression>),
-    Binary(Box<Binary>),
-    Assignment(Symbol, Box<Expression>),
-    Def(Symbol, Vec<Symbol>, Box<Expression>),
-    Parenthesised(Vec<Expression>),
-    Block(Vec<Expression>),
+pub struct Function {
+    pub identifier: Option<Symbol>,
+    pub arguments: Vec<Symbol>,
+    pub block: Block,
 }
-
-impl Debug for Expression {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Self::Def(name, args, body) => {
-                f.write_fmt(format_args!("{:?}({:?}){:?}", name, args, body))
-            }
-            Self::Literal(l) => f.write_fmt(format_args!("L|{:?}|", l)),
-            Self::Binary(b) => f.write_fmt(format_args!("{:?}", b)),
-            Self::Call(s, es) => f.write_fmt(format_args!("Call({:?}, {:?})", s, es)),
-            Self::Parenthesised(p) => f.write_fmt(format_args!("Parens({:?})", p)),
-            Self::Block(b) => f.write_fmt(format_args!("Block{{{:?}}}", b)),
-            Self::Assignment(symbol, exp) => f.write_fmt(format_args!("{:?} = {:?}", symbol, exp)),
-        }
-    }
-}
-
-impl Expression {
-    pub fn simplify(self) -> Self {
-        match self {
-            Expression::Binary(b) => b.simplify(),
-            Expression::Block(es) => {
-                Expression::Block(es.into_iter().map(|s| s.simplify()).collect())
-            }
-            x => x,
-        }
-    }
+#[derive(Debug, PartialEq)]
+pub struct Assignment {
+    pub identifier: Symbol,
+    pub value: Expression,
 }
 
 #[derive(Debug, PartialEq)]
-pub enum Statement {
-    Assignment(Symbol, Box<Expression>),
-    Print(Box<Expression>),
+pub enum Literal {
+    Nil,
+    Number(f64),
+    String(String),
+    Boolean(bool),
+    Symbol(Symbol),
+}
+#[derive(Debug, PartialEq)]
+pub struct Parenthesised(pub Vec<Expression>);
+#[derive(Debug, PartialEq)]
+pub struct Block(pub Vec<Expression>);
+#[derive(Debug, PartialEq)]
+pub struct Call {
+    pub function: Expression,
+    pub arguments: Vec<Expression>,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, PartialEq)]
+pub struct Cond {
+    pub conditions: Vec<(Expression, Expression)>,
+    pub otherwise: Option<Expression>,
+}
 
-    #[test]
-    fn binary_simplifies_correctly() {
-        assert_eq!(
-            Binary::Plus(vec![Expression::Binary(Box::new(Binary::And(vec![
-                Expression::Literal(Literal::Double(3.0))
-            ])))])
-            .simplify(),
-            Expression::Literal(Literal::Double(3.0))
-        );
-    }
-
-    #[test]
-    fn nested_binary_simplifies_correctly() {
-        assert_eq!(
-            Binary::And(vec![
-                Expression::Binary(Box::new(Binary::Plus(vec![Expression::Literal(
-                    Literal::Double(3.0)
-                )]))),
-                Expression::Binary(Box::new(Binary::Plus(vec![Expression::Literal(
-                    Literal::Double(3.0)
-                )])))
-            ])
-            .simplify(),
-            Expression::Binary(Box::new(Binary::And(vec![
-                Expression::Literal(Literal::Double(3.0)),
-                Expression::Literal(Literal::Double(3.0))
-            ])))
-        )
-    }
+#[derive(Debug, PartialEq)]
+pub enum Expression {
+    Call(Box<Call>),
+    Parenthesised(Box<Parenthesised>),
+    Cond(Box<Cond>),
+    Block(Box<Block>),
+    Assignment(Box<Assignment>),
+    Function(Box<Function>),
+    Literal(Literal),
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,19 +1,21 @@
-use crate::ast::{Binary, Expression, Literal, Parenthesised, Symbol};
 use nom::{
-    bytes::complete::{tag, take_while_m_n},
-    character::{
-        complete::{alpha0, alpha1, alphanumeric0, multispace0, space0, space1},
-        is_alphabetic,
-    },
-    combinator::verify,
+    bytes::complete::{tag, take_while, take_while1},
+    character::complete::{multispace0, multispace1},
+    combinator::opt,
     error::ParseError,
-    multi::{many0, many1, separated_list0},
-    number,
+    multi::{many1, separated_list0, separated_list1},
     number::complete::double,
-    sequence::preceded,
-    sequence::{delimited, tuple},
-    IResult, InputLength, Parser,
+    sequence::{delimited, preceded, tuple},
+    AsChar, IResult, InputLength, InputTakeAtPosition, Parser,
 };
+
+use crate::ast::{
+    Assignment, Block, Call, Cond, Expression, Function, Literal, Parenthesised, Symbol,
+};
+
+const RESERVED_STRINGS: &[&str] = &["=>", "let", "cond", "fn"];
+
+const RESERVED_CHARS: &[char] = &['(', ')', '{', '}', '.', ';', ',', '!'];
 
 fn nil(input: &str) -> IResult<&str, Literal> {
     let (s, _) = tag("nil")(input)?;
@@ -22,160 +24,340 @@ fn nil(input: &str) -> IResult<&str, Literal> {
 
 fn false_lit(input: &str) -> IResult<&str, Literal> {
     let (s, _) = tag("false")(input)?;
-    Ok((s, Literal::False))
+    Ok((s, Literal::Boolean(false)))
 }
 
 fn true_lit(input: &str) -> IResult<&str, Literal> {
     let (s, _) = tag("true")(input)?;
-    Ok((s, Literal::True))
+    Ok((s, Literal::Boolean(true)))
 }
 
 fn number(input: &str) -> IResult<&str, Literal> {
     let (s, n) = double(input)?;
-    Ok((s, Literal::Double(n)))
+    Ok((s, Literal::Number(n)))
+}
+
+fn symbol(input: &str) -> IResult<&str, Symbol> {
+    let (s, first) = take_while1(|x: char| {
+        !(x.is_whitespace() | x.is_digit(10) | RESERVED_CHARS.iter().any(|&c| c == x))
+    })(input)?;
+    let (s, rest) =
+        take_while(|x: char| !(x.is_whitespace() | RESERVED_CHARS.iter().any(|&c| c == x)))(s)?;
+    let full_string = format!("{first}{rest}");
+    if RESERVED_STRINGS.iter().any(|&x| x == full_string) {
+        Err(nom::Err::Error(nom::error::Error::from_error_kind(
+            input,
+            nom::error::ErrorKind::Char,
+        )))
+    } else {
+        Ok((s, Symbol(full_string)))
+    }
+}
+
+fn surrounded_list<I, O, O1, O2, O3, E, F, G, H, J>(
+    mut first: F,
+    mut separator: G,
+    mut value: H,
+    mut last: J,
+) -> impl FnMut(I) -> IResult<I, Vec<O>, E>
+where
+    I: InputTakeAtPosition + Clone + InputLength,
+    <I as InputTakeAtPosition>::Item: AsChar + Clone,
+    H: Parser<I, O, E>,
+    F: Parser<I, O1, E>,
+    G: Parser<I, O2, E>,
+    J: Parser<I, O3, E>,
+    E: ParseError<I>,
+{
+    move |s| {
+        let (s, _) = first.parse(s)?;
+        let (s, _) = multispace0(s)?;
+        let (s, values) = separated_list0(
+            many1(delimited(multispace0, |x| separator.parse(x), multispace0)),
+            |x| value.parse(x),
+        )(s)?;
+        let (s, _) = multispace0(s)?;
+        let (s, _) = last.parse(s)?;
+        Ok((s, values))
+    }
+}
+
+/// Delimite the given parser by spaces
+fn s_d<I, O, E, F>(p: F) -> impl FnMut(I) -> IResult<I, O, E>
+where
+    I: InputTakeAtPosition + Clone + InputLength,
+    <I as InputTakeAtPosition>::Item: AsChar + Clone,
+    F: Parser<I, O, E>,
+    E: ParseError<I>,
+{
+    delimited(multispace0, p, multispace0)
+}
+
+/// A function that returns a list separated and possibly delimited by the separator.
+/// The separator has to occur at least once, and can occur multiple times
+/// Returns the vector of values in the list, and also a boolean which is true if the
+/// function ended with the separator
+fn separated_delimited_list0<I, O, O2, E, F, G>(
+    mut sep: G,
+    mut f: F,
+) -> impl FnMut(I) -> IResult<I, (Vec<O>, bool), E>
+where
+    I: Clone + InputLength,
+    F: Parser<I, O, E>,
+    G: Parser<I, O2, E>,
+    E: ParseError<I>,
+{
+    move |s| {
+        let (s, _) = opt(many1(|x| sep.parse(x)))(s)?;
+        let (s, items) = separated_list0(many1(|x| sep.parse(x)), |x| f.parse(x))(s)?;
+        let (s, last) = opt(many1(|x| sep.parse(x)))(s)?;
+        Ok((s, (items, last.is_some())))
+    }
+}
+
+fn function(input: &str) -> IResult<&str, Function> {
+    let (s, _) = tag("fn")(input)?;
+    let (s, fn_symbol) = s_d(opt(symbol))(s)?;
+    let (s, args) = opt(delimited(
+        tag("("),
+        separated_delimited_list0(s_d(tag(",")), symbol),
+        tag(")"),
+    ))(s)?;
+    let (s, _) = multispace0(s)?;
+    let (s, b) = block(s)?;
+    Ok((
+        s,
+        Function {
+            identifier: fn_symbol,
+            arguments: args.map(|(a, _)| a).unwrap_or_else(Vec::new),
+            block: b,
+        },
+    ))
 }
 
 fn literal(input: &str) -> IResult<&str, Literal> {
-    nil.or(false_lit)
+    nil.or(number)
         .or(true_lit)
-        .or(number)
+        .or(false_lit)
         .or(symbol.map(Literal::Symbol))
         .parse(input)
 }
 
-fn pexpression(input: &str) -> IResult<&str, Expression> {
-    delimited(tag("("), expression, tag(")"))(input)
+fn parenthesised(input: &str) -> IResult<&str, Parenthesised> {
+    let (s, exps) = delimited(tag("("), expressions, tag(")"))(input)?;
+    Ok((s, Parenthesised(exps)))
 }
 
-fn bin<'a>(input: &'a str, op: &str) -> IResult<&'a str, ()> {
-    let (s, _) = delimited(multispace0, tag(op), multispace0)(input)?;
-    Ok((s, ()))
+fn block(input: &str) -> IResult<&str, Block> {
+    let (s, exps) = delimited(tag("{"), expressions, tag("}"))(input)?;
+    Ok((s, Block(exps)))
 }
 
-fn bin_op<'a>(input: &'a str, operators: &[&str]) -> IResult<&'a str, Expression> {
-    match operators {
-        [] => pexpression
-            .or(block)
-            .or(call)
-            .or(literal.map(Expression::Literal))
-            .parse(input),
-        [a, rest @ ..] => {
-            let (s, first_exp) = bin_op(input, rest)?;
-            let (s, rest_exps) = many0(tuple((|x| bin(x, a), |x| bin_op(x, rest))))(s)?;
-            let mut v = vec![first_exp];
-            v.extend(rest_exps.into_iter().map(|(_, x)| x));
-            Ok((
-                s,
-                Expression::Binary(Box::new(Binary::from_op_string(a, v))),
-            ))
-        }
-    }
+fn l1(input: &str) -> IResult<&str, Expression> {
+    block
+        .map(|x| Expression::Block(Box::new(x)))
+        .or(parenthesised.map(|x| Expression::Parenthesised(Box::new(x))))
+        .or(function.map(|x| Expression::Function(Box::new(x))))
+        .or(literal.map(Expression::Literal))
+        .parse(input)
 }
 
-fn symbol(input: &str) -> IResult<&str, Symbol> {
-    let (s, f) = alpha1(input)?;
-    let (s, rest) = alphanumeric0(s)?;
-    Ok((s, Symbol(format!("{f}{rest}"))))
+fn no_arg_call(input: &str) -> IResult<&str, Call> {
+    let (s, e) = l1(input)?;
+    let (s, _) = tuple((multispace0, tag("!")))(s)?;
+    Ok((
+        s,
+        Call {
+            function: e,
+            arguments: vec![],
+        },
+    ))
 }
 
-fn assignment(input: &str) -> IResult<&str, Expression> {
-    let (s, sym) = symbol(input)?;
-    let (s, _) = delimited(space0, tag("="), space0)(s)?;
-    let (s, e) = expression(s)?;
-    Ok((s, Expression::Assignment(sym, Box::new(e))))
+fn l2(input: &str) -> IResult<&str, Expression> {
+    no_arg_call
+        .map(|x| Expression::Call(Box::new(x)))
+        .or(l1)
+        .parse(input)
 }
 
-fn arguments(input: &str) -> IResult<&str, Vec<Expression>> {
-    delimited(
-        tag("("),
-        separated_list0(delimited(multispace0, tag(","), multispace0), expression),
-        tag(")"),
-    )(input)
+fn infix_call(input: &str) -> IResult<&str, Call> {
+    let (s, arg) = l2(input)?;
+    let (s, _) = s_d(tag("."))(s)?;
+    let (s, fun) = l2(s)?;
+    let (s, (mut other_args, _)) = separated_delimited_list0(multispace1, l2)(s)?;
+    let mut args = vec![arg];
+    args.append(&mut other_args);
+    Ok((
+        s,
+        Call {
+            function: fun,
+            arguments: args,
+        },
+    ))
 }
 
-fn call(input: &str) -> IResult<&str, Expression> {
-    let (s, f) = symbol(input)?;
-    let (s, args) = preceded(space0, arguments)(s)?;
-    Ok((s, Expression::Call(f, args)))
+fn l3(input: &str) -> IResult<&str, Expression> {
+    infix_call
+        .map(|x| Expression::Call(Box::new(x)))
+        .or(l2)
+        .parse(input)
 }
 
-fn block(input: &str) -> IResult<&str, Expression> {
-    let (s, v) = delimited(
-        tag("{"),
-        separated_list0(delimited(multispace0, tag(";"), multispace0), expression),
-        tag("}"),
-    )(input)?;
-    Ok((s, Expression::Block(v)))
+fn normal_call(input: &str) -> IResult<&str, Call> {
+    let (s, f) = l3(input)?;
+    let (s, _) = multispace1(s)?;
+    let (s, args) = separated_list1(multispace1, l3)(s)?;
+    Ok((
+        s,
+        Call {
+            function: f,
+            arguments: args,
+        },
+    ))
+}
+
+fn l4(input: &str) -> IResult<&str, Expression> {
+    normal_call
+        .map(|x| Expression::Call(Box::new(x)))
+        .or(l3)
+        .parse(input)
+}
+
+fn assignment(input: &str) -> IResult<&str, Assignment> {
+    let (s, _) = tag("let")(input)?;
+    let (s, id) = s_d(symbol)(s)?;
+    let (s, ex) = expression(s)?;
+    Ok((
+        s,
+        Assignment {
+            identifier: id,
+            value: ex,
+        },
+    ))
+}
+
+fn cond(input: &str) -> IResult<&str, Cond> {
+    let (s, _) = tag("cond")(input)?;
+    let (s, _) = s_d(tag("{"))(s)?;
+    let (s, (exps, _)) = separated_delimited_list0(
+        s_d(tag(",")),
+        tuple((expression, s_d(tag("=>")), expression)).map(|(x1, _, x2)| (x1, x2)),
+    )(s)?;
+    let (s, last_expression) = opt(expression)(s)?;
+    let (s, _) = multispace0(s)?;
+    let (s, _) = tag("}")(s)?;
+    Ok((
+        s,
+        Cond {
+            conditions: exps,
+            otherwise: last_expression,
+        },
+    ))
 }
 
 fn expression(input: &str) -> IResult<&str, Expression> {
-    block
-        .or(pexpression)
-        .or(assignment)
-        .or(call)
-        .or(|x| bin_op(x, Binary::ops()))
-        .or(literal.map(Expression::Literal))
-        .map(|x| x.simplify())
+    assignment
+        .map(|x| Expression::Assignment(Box::new(x)))
+        .or(cond.map(|x| Expression::Cond(Box::new(x))))
+        .or(l4)
+        .parse(input)
+}
+
+fn expressions(input: &str) -> IResult<&str, Vec<Expression>> {
+    separated_delimited_list0(s_d(tag(";")), expression)
+        .map(|(x, _)| x)
         .parse(input)
 }
 
 #[cfg(test)]
 mod tests {
 
-    use nom::multi::separated_list1;
-
     use super::*;
     use crate::ast::Literal;
 
     #[test]
-    fn full_expression() {
+    fn test_expressions() {
         assert_eq!(
-            expression("2+(3+x) and (x = 5) and c(x, y, z, 10, f=4, {a=1;b=3}, c=2)"),
-            Ok(("", Expression::Literal(Literal::Double(3.0))))
-        );
-    }
-
-    #[test]
-    fn test_separated() {
-        assert_eq!(
-            separated_list1(
-                delimited(space0, tag(";"), space0),
-                assignment
-                    .or(|x| bin_op(x, Binary::ops()))
-                    .map(|x| x.simplify())
-            )("a=2; b=3; 30"),
-            Ok(("", vec![]))
-        );
-    }
-
-    #[test]
-    fn test_arguments() {
-        assert_eq!(
-            call("af4(a=3, b=4)"),
-            Ok(("", Expression::Call(Symbol("hi".into()), vec![])))
-        );
-    }
-
-    #[test]
-    fn general_parsing_test() {
-        assert_eq!(
-            bin_op("1 and (3 + 4) and 2", Binary::ops()).map(|(s, b)| (s, b.simplify())),
-            Ok(("", Expression::Literal(Literal::Double(0.0))))
-        );
+            expressions(";;\n\n2\n;; 3.1; let x \n\n s 4"),
+            Ok((
+                "",
+                vec![
+                    Expression::Literal(Literal::Number(2.0)),
+                    Expression::Literal(Literal::Number(3.1)),
+                    Expression::Assignment(Box::new(Assignment {
+                        identifier: Symbol("x".into()),
+                        value: Expression::Call(Box::new(Call {
+                            function: Expression::Literal(Literal::Symbol(Symbol("s".into()))),
+                            arguments: vec![Expression::Literal(Literal::Number(4.0))]
+                        }))
+                    }))
+                ]
+            ))
+        )
     }
 
     #[test]
     fn test_symbol() {
-        assert_eq!(symbol("syyy"), Ok(("", Symbol("syyy".into()))));
-        assert_eq!(symbol("s23!"), Ok(("!", Symbol("s23".into()))));
-        assert!(symbol("31!").is_err());
+        assert_eq!(
+            symbol("hello_there"),
+            Ok(("", Symbol("hello_there".into())))
+        );
+        assert_eq!(symbol("a symbol"), Ok((" symbol", Symbol("a".into()))));
+        assert!(symbol("2start").is_err());
+        assert!(symbol("let").is_err());
+        assert_eq!(symbol("=23"), Ok(("", Symbol("=23".into()))));
+        assert_eq!(symbol("=2;!3"), Ok((";!3", Symbol("=2".into()))));
     }
 
     #[test]
-    fn test_assignment() {
+    fn test_parsing_function() {
         assert_eq!(
-            assignment("a =    3"),
-            Ok(("", Expression::Literal(Literal::Double(3.0))))
+            function("fn x (a,b,c) {x.* a}"),
+            Ok((
+                "",
+                Function {
+                    identifier: Some(Symbol("x".into())),
+                    arguments: vec![Symbol("a".into()), Symbol("b".into()), Symbol("c".into())],
+                    block: Block(vec![Expression::Call(Box::new(Call {
+                        function: Expression::Literal(Literal::Symbol(Symbol("*".into()))),
+                        arguments: vec![
+                            Expression::Literal(Literal::Symbol(Symbol("x".into()))),
+                            Expression::Literal(Literal::Symbol(Symbol("a".into())))
+                        ]
+                    }))])
+                }
+            ))
         )
+    }
+
+    #[test]
+    fn test_surrounded_list() {
+        assert_eq!(
+            separated_delimited_list0(delimited(multispace0, tag(","), multispace0), number)
+                .parse(",, , ,,,,2,, 3, 4, 5 ,,, , ,,,"),
+            Ok((
+                "",
+                (
+                    vec![
+                        Literal::Number(2.0),
+                        Literal::Number(3.0),
+                        Literal::Number(4.0),
+                        Literal::Number(5.0)
+                    ],
+                    true
+                )
+            ))
+        );
+        assert_eq!(
+            separated_delimited_list0(delimited(multispace0, tag(","), multispace0), number)
+                .parse(",,,"),
+            Ok(("", (vec![], false)))
+        );
+    }
+
+    #[test]
+    fn test_cond() {
+        assert!(cond("cond {x => 2,    , , , 2 .+ 3 => 3,, x .* 30 }").is_ok(),)
     }
 }
